6b15a3c24d9bc535e840c4bd1efa4cd3282d1076
Alanna Bouloy <lanihope@gmail.com>
2020-10-05 16:55:33 -0400

    added api fetch statement

--- src/App.js
+++ src/App.js
@@ -5,16 +5,38 @@ import Filter from './Filter';
 import BookList from './BookList';
 import BookItem from './BookItem';
 
-function App() {
-  return (
-    <main className='App'>
-      <Header />
-      <SearchBar />
-      <Filter />
-      <BookList />
-      <BookItem />
-    </main>
-  );
+class App extends React.Component {
+  state = {
+    bookList: [],
+    loading: false,
+    filterSelect: null,
+    printTypeSelect: null,
+  }
+
+  BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=f';
+  API_Key = '&key=AIzaSyBTmhbI8R9kc6tGeRmx2IZMfvaZYnrgzSM';
+
+  componentDidMount(){
+    const keyword = getKeyword();
+    fetch(`${this.BASE_URL}${keyword}${this.API_Key}`)
+      .then(resp => resp.json)
+      .then(data => {
+        console.log(data)
+    })
+  }
+  
+  render(){
+    return (
+      <main className='App'>
+        <Header />
+        <SearchBar />
+        <Filter />
+        <BookList />
+        <BookItem />
+      </main>
+    );
+  }
+  
 }
 
 export default App;
