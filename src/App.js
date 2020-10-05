0d8f48b6a18bc2beafbe30715e1476f1c55ee432
Alanna Bouloy <lanihope@gmail.com>
2020-10-05 12:16:20 -0400

    header css added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <Header />
     </main>
   );
 }
