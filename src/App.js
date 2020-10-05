bad42b359389ddc54ed950d50f9b22282611e9d7
Alanna Bouloy <lanihope@gmail.com>
2020-10-05 12:31:36 -0400

    added SearchBard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import SearchBar from './SearchBar';
 
 function App() {
   return (
     <main className='App'>
       <Header />
+      <SearchBar />
     </main>
   );
 }
---
+++ src/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+    render() {
+        return (
+            <div className = 'search'>
+                <form id='js-search-bar'>
+                    <label for='search'>Search: </label>
+                    <input id='search' type='text' />
+                    <input id= 'js-submit' type='submit' value="Search"/>
+                </form>
+            </div>
+        )
+    }
+}
